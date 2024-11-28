d6d936cd79eb62981a14c67a14e25eab8a7b39bf
JL0424 <1710715380@qq.com>
2024-11-28 15:54:41 +0800

    Create __init__.py

---
+++ modeling/__init__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+from .MAFFN import Baseline
+
+
+def build_model(cfg, num_classes):
+    model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NAME,
+                     cfg.MODEL.GENERALIZED_MEAN_POOL, cfg.MODEL.PRETRAIN_CHOICE)
+    return model
